b9af06c6fd5c0bbe9815d49525dd3416e8972ebc
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2024-04-11 19:43:28 -0700

    uploaded user selected image to imgur

--- sherlock/image_search.py
+++ sherlock/image_search.py
@@ -3,6 +3,7 @@
 import sys
 from imgurpython import ImgurClient as imgur
 import requests
+import base64
 
 # from google_images_search import GoogleImagesSearch
 
@@ -12,13 +13,23 @@ import requests
 
 client_id = 'f0af1953936b647'
 client_secret = '15cf26798f8c0cdadf9953f82255a5c08e17f379'
+file_path = 'C:\\Users\\BryanPC\\Downloads\\wombatpic.jpg'
 
 def main(): 
+    encoded_string = None
+
+    with open(file_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
     headers = {
-        'Authorization': f"Client-ID {{{{{client_id}}}}}"
+        'Authorization': 'Client-ID {}'.format(client_id)
     }
+
+    print(headers)
+    print(encoded_string)
     print("uploading")
-    res = requests.post("https://api.imgur.com/3/image", headers=headers, data={"type": "image", "title": "Wombat testing upload", "image": "C:\\Users\\BryanPC\\Downloads\\wombatpic.png"})
+    res = requests.post(url="https://api.imgur.com/3/image", headers=headers, data={
+        "image": encoded_string
+    })
     print(res.json())
 
     
