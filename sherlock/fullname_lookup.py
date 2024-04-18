9cdb4086dc475217395a9df82c149ffbc3f59641
Roger <sirsalty@DESKTOP-PKKS0BE>
2024-04-18 00:15:14 -0700

    Information is written into a .txt file

--- sherlock/fullname_lookup.py
+++ sherlock/fullname_lookup.py
@@ -3,12 +3,19 @@
 #               pip install google
 # Usage: python3 fullname_lookup.py "fullname" 
 import sys
+import os
 from googlesearch import search
 
 if __name__ == "__main__":
+    if os.path.exists("fullname-search.txt"):
+        os.remove("fullname-search.txt")
+
     # Setting command line arguments to variables
     fullname = sys.argv[1]
+    file = open("fullname-search.txt", "w")
+
     
     for j in search(fullname, tld="co.in", num=30, stop=15, pause=2):
-        print(j)
+        file.write(j + "\n")
 
+    file.close()
