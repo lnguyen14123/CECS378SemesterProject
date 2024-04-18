72f41ad04e0789d3ff848736dec7e17828a8a7a5
TheAlex7 <144410177+TheAlex7@users.noreply.github.com>
2024-04-18 01:57:48 -0700

    added readme for installation and tweaked install shell file

--- Linux_Package_Install_Files/install.sh
+++ Linux_Package_Install_Files/install.sh
@@ -1,7 +1,8 @@
+sudo apt update
 dpkg-source -x sherlock_0.14.3+git20240315.55c680f-1.dsc
-pip3 install -r sherlock-0.14.3+git20240315.55c680f/requirements.txt
-sudo apt-get build-dep sherlock-0.14.3+git20240315.55c680f
 cd sherlock-0.14.3+git20240315.55c680f
+sudo apt-get build-dep .
+pip3 install -r requirements.txt
 dpkg-buildpackage -rfakeroot -uc -us
 cd ..
 sudo dpkg -i sherlock_0.14.3+git20240315.55c680f-1_all.deb
