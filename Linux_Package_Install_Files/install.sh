20d0922dbf1390617c47f95a0c2b0182e3797f27
TheAlex7 <144410177+TheAlex7@users.noreply.github.com>
2024-04-19 19:46:37 -0700

    added comments to Linux install.sh file

--- Linux_Package_Install_Files/install.sh
+++ Linux_Package_Install_Files/install.sh
@@ -1,8 +1,23 @@
+#must update apt after changing deb-src
 sudo apt update
+
+#compile the source files for the package
 dpkg-source -x sherlock_0.14.3+git20240315.55c680f-1.dsc
+
+#change director to root dir of source files
 cd sherlock-0.14.3+git20240315.55c680f
+
+#build dependencies for package as stated in debian/control file
 sudo apt-get build-dep .
+
+#install python run-time dependencies
 pip3 install -r requirements.txt
+
+#build an unsigned version of the package. along with fakeroot as to not alter system files by accident
 dpkg-buildpackage -rfakeroot -uc -us
+
+# return to original dir
 cd ..
+
+#install the built package
 sudo dpkg -i sherlock_0.14.3+git20240315.55c680f-1_all.deb
