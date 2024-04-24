8ac398a9b01983b7d696a684a3854536c8ca14dd
Alex <alexog404@gmail.com>
2024-04-24 14:25:43 -0700

    fixed minor bug in wordlist gen

--- sherlock/wordlist_generator.py
+++ sherlock/wordlist_generator.py
@@ -26,8 +26,8 @@ def generate_passwords(wmap, filepath):
     with open(filepath, 'a') as file:
         for wrd in wmap.keys():
             file.write(f"{wrd}\n") #write base word
-            file.write(f"{wrd[0].upper()+wrd[1:]}") #capital only
-            
+            file.write(f"{wrd[0].upper()+wrd[1:]}\n") #capital only
+
             for i in range(10): # write all words starting with uppercase and ending with each digit
                 upper = wrd[0].upper() + wrd[1:]
                 file.write(f"{upper}{i}\n")
