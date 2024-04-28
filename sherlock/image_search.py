dd2b33b61c03809b6b6f7477cb27b2a4fee76212
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2024-04-28 16:02:47 -0700

    generated website list from image

--- sherlock/image_search.py
+++ sherlock/image_search.py
@@ -3,6 +3,7 @@
 import sys
 from imgurpython import ImgurClient as imgur
 import requests
+from serpapi import GoogleSearch
 import base64
 
 # from google_images_search import GoogleImagesSearch
@@ -11,12 +12,20 @@ import base64
 #Imgur Secret: 15cf26798f8c0cdadf9953f82255a5c08e17f379
 #Endpoint: https://api.imgur.com/3/image
 
+#Serp API Key: abdc4ceec32ee7065547ea930bc8b064f1fa7f6ff51fc46ad0246e682e82a160
+#Endpoint: https://serpapi.com/search?engine=google_reverse_image
+
 client_id = 'f0af1953936b647'
 client_secret = '15cf26798f8c0cdadf9953f82255a5c08e17f379'
-file_path = 'C:\\Users\\BryanPC\\Downloads\\wombatpic.jpg'
+file_path = 'C:\\Users\\BryanPC\\Downloads\\bird.png'
 
-def main(): 
+def print_dictionary(dictionary):
+    for key, value in dictionary.items():
+        print(f"{key}: {value}")
+
+def main(output_path='website_list.txt'): 
     encoded_string = None
+    websiteList = []
 
     with open(file_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
@@ -30,9 +39,35 @@ def main():
     res = requests.post(url="https://api.imgur.com/3/image", headers=headers, data={
         "image": encoded_string
     })
-    print(res.json())
+    resUpload = res.json()
+    searchLink = resUpload['data']['link']
+    print(searchLink)
 
-    
+    params = {
+        "engine": "google_reverse_image",
+        "image_url": searchLink,
+        "api_key": "abdc4ceec32ee7065547ea930bc8b064f1fa7f6ff51fc46ad0246e682e82a160"
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+    image_results = results["image_results"]
+
+    print(type(image_results))
+
+    for dictionary in image_results:
+        if "link" in dictionary:
+            print(dictionary["link"])
+            websiteList.append(dictionary["link"])
+        if "sitelinks" in dictionary:
+            for linkdict in dictionary["sitelinks"]["list"]:
+                websiteList.append(linkdict["link"])
+                print(linkdict["link"])
+
+    with open(output_path, 'w') as file:
+        for site in websiteList:
+            file.write(f"{site}\n") #write base word
+        file.close()
 if __name__ == "__main__":
     main()
 
