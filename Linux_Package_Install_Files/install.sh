810607db019880cebb993a612a463d6dec815aaa
Alex <alexog404@gmail.com>
2024-04-27 18:53:25 -0700

    fixed misspelling in a comment

--- Linux_Package_Install_Files/install.sh
+++ Linux_Package_Install_Files/install.sh
@@ -4,7 +4,7 @@ sudo apt update
 #compile the source files for the package
 dpkg-source -x sherlock_0.14.3+git20240315.55c680f-1.dsc
 
-#change director to root dir of source files
+#change directory to root dir of source files
 cd sherlock-0.14.3+git20240315.55c680f
 
 #build dependencies for package as stated in debian/control file
